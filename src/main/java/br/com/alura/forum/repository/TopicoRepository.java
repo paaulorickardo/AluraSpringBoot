2d61574219bfbe954de88f4a917a9b96f7c4bd21
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-09-22 15:24:39 -0300

    Ajuste no repository

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 //import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +10,8 @@ import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
-//	List<Topico> findByCursoNome(String nomeCurso);
-	
+	List<Topico> findByCursoNome(String nomeCurso); //se por acaso tiver um nome parecido dentro da entidade usar Curso_Nome
+	/*outra forma personalizada
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :=nomeCurso")
+	List<Topico> carregarPorNomePersonalizado(@Param("nomeCurso") String nomeCurso);*/
 }
