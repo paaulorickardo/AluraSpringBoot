f56afb288eb07c3d665fb9d86ede2fd9c98f34ca
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-09-26 15:53:00 -0300

    Validações com Bean Validation

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,12 +40,11 @@ public class TopicosController {
 			return TopicoDto.converter(topicos);
 		}
 	}
-	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+	@PostMapping                      //@Valid quando for puxar as informações rode as validações TopicoForm
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-		
-		
+			
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri(); //criando uma uri dinamica
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 		
